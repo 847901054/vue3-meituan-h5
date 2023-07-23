8bb4f9dd915e75e4494a32d58559028c8df429c7
859284708 <zbt859284708>
2023-07-23 20:21:50 +0800

    vue-cli5.x降级为4.x

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  css: {
+    loaderOptions: {
+      postcss: {
+        plugins: [
+          require('postcss-pxtorem')({ // 把px单位换算成rem单位
+            rootValue: 16,
+            propList: ['*']
+          })
+        ]
+      }
+    }
+  }
+}
